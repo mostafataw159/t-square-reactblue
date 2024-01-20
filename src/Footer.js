a9439b05b814283591acebb245350f4a2874c9bd
mostafataw159 <152802283+mostafataw159@users.noreply.github.com>
2024-01-20 03:11:41 +0200

    Add files via upload

---
+++ src/Footer.js
@@ -0,0 +1,18 @@
+
+
+function footer(){
+    return(
+<div>
+<footer className="text-center p-4">
+    <div className="contianer">
+        <div className="row">
+            <div className="col-md-12">
+                <h5>© Your Website 2022. All Rights Reserved.</h5>
+            </div>
+        </div>
+    </div>
+</footer>
+</div>
+    )
+}
+export default footer;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Ali from './App';
+
+
+const hassen = ReactDOM.createRoot(document.getElementById('root'));
+hassen.render(<Ali />);
+
+
